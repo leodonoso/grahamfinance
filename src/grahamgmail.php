e070da29e8c2e8694f04158ef957bee55d8fa061
Pancakes <leonardoadonosog@gmail.com>
2025-01-08 20:35:40 -0400

    Managed to Filter Emails by Transactions Only

--- src/grahamgmail.php
+++ src/grahamgmail.php
@@ -22,7 +22,7 @@ use Google\Service\Gmail;
             } else {
                 $labelsHTML.="<p>Labels</p>";
                 foreach ($results->getLabels() as $label) {
-                    $labelsHTML.="<p>".$label->getName()."</p>";
+                    $labelsHTML.="<p>".$label->getId()."</p>";
                 }
             }
 
@@ -36,6 +36,7 @@ use Google\Service\Gmail;
             $messages = array();
             $opt_params = array();
             $i=0;
+            $opt_params['labelIds'] = array('Label_8449633831705723059');
 
             do {
                 if ($i==5) break;
@@ -57,27 +58,34 @@ use Google\Service\Gmail;
 
             $j = 0;
             foreach ($messages as $message) {
-                echo 'Message with ID: ' . $message->getId() . '<br/>';
                 $message_get_res = $service->users_messages->get($user, $message->getId());
-                $message_parts = $message_get_res->payload->parts;
-                if (count($message_parts) > 0) {
-                    $message_data = $message_parts[1]->body->data;
-                }
-                else {
-                    $message_data = $message_get_res->payload->body->data;
+                $msg_snippet = strtoupper($message_get_res->getSnippet());
+                $keywords = array("DEPOSIT", "BINANCE PAY", "WITHDRAWAL", "PUNTOS DE VENTA", "PAGO A UN", "PAGO DE SERVICIOS");
+                $is_transaction = false;
+
+                foreach ($keywords as $keyword) {
+                    if (str_contains($msg_snippet, $keyword)) {
+                        $is_transaction = true;
+                    };
                 }
 
-                // echo "<pre>".var_export($message_get_res->payload->parts[1]->body->data, true)."</pre>";
-                // echo "<pre>".var_export($message_data, true)."</pre>";
-                $decoded_email = str_replace("-", "+", $message_data);
-                $decoded_email = str_replace("_", "/", $decoded_email);
-                echo base64_decode($decoded_email);
+                if ($is_transaction) {
+                    $message_parts = $message_get_res->payload->parts;
+                    if (count($message_parts) > 0) {
+                        $message_data = $message_parts[1]->body->data;
+                    }
+                    else {
+                        $message_data = $message_get_res->payload->body->data;
+                    }
+    
+                    $decoded_email = str_replace("-", "+", $message_data);
+                    $decoded_email = str_replace("_", "/", $decoded_email);
+                    echo base64_decode($decoded_email);
+                }     
 
                 $j++;
-                if ($j == 10) { break; }
+                if ($j == 50) { break; }
             }
-
-            return $messages;
         }
     }
 ?>
